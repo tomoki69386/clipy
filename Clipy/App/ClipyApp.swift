354ebb096adacf482a484c380778235bc284529a
tomoki_sun <tomoki69386@gmail.com>
2020-11-15 18:08:20 +0900

    Create Clipy

---
+++ Clipy/App/ClipyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipyApp.swift
+//  Clipy
+//
+//  Created by 築山朋紀 on 2020/11/15.
+//
+
+import SwiftUI
+
+@main
+struct ClipyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Clipy/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Clipy
+//
+//  Created by 築山朋紀 on 2020/11/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
